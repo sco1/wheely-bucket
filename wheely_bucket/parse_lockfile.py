f052a78673178b5bab8f89f77df905dc8b0acb3e
S. Co1 <sco1.git@gmail.com>
2025-09-01 16:55:55 -0400

    Refactor lockfile parsing
    Extract version identification, as we'll be leaning on `pip download` rather than downloading things ourselves

--- wheely_bucket/parse_lockfile.py
+++ wheely_bucket/parse_lockfile.py
@@ -4,61 +4,47 @@ import tomllib
 import typing as t
 from pathlib import Path
 
-from packaging import tags
 
-
-class WheelSpec(t.NamedTuple):  # noqa: D101
+class PackageSpec(t.NamedTuple):  # noqa: D101
     package_name: str
-    wheel_name: str
-    url: str
-    platforms: frozenset[str]
+    locked_ver: str
+
+    def __str__(self) -> str:
+        return self.spec
+
+    @property
+    def spec(self) -> str:
+        """Return a `pip`-compatible package+version specification."""
+        return f"{self.package_name}=={self.locked_ver}"
 
     @classmethod
-    def from_url(cls, url: str) -> WheelSpec:
-        """
-        Build a `WheelSpec` instance from the provided wheel download URL.
+    def from_string(cls, spec_str: str) -> PackageSpec:
+        raise NotImplementedError
 
-        URLs are assumed to be of the form `"https://<CDN component>/<wheel name>"`, e.g.
-        `"https://<CDN component>/flake8_annotations-3.1.1-py3-none-any.whl"`
-
-        Platform tags are parsed using the `packaging` library & stored to assist with downstream
-        filtering.
-        """
-        *_, wname = url.split("/")
-        pname, _, raw_tag = wname.split("-", maxsplit=2)
-        parsed_tag = tags.parse_tag(raw_tag.removesuffix(".whl"))
-
+    @classmethod
+    def from_lock(cls, locked_info: dict[str, t.Any]) -> PackageSpec:
+        """Build a `PackageSpec` instance from a package's `uv.lock` metadata."""
         return cls(
-            package_name=pname,
-            wheel_name=wname,
-            url=url,
-            platforms=frozenset(p.platform for p in parsed_tag),
+            package_name=locked_info["name"],
+            locked_ver=locked_info["version"],
         )
 
+    @classmethod
+    def from_lockfile(cls, lockfile: Path, exclude_editable: bool = True) -> set[PackageSpec]:
+        """
+        Parse all resolved package metadata from `uv.lock` into a set of `PackageSpec` instances.
 
-def parse_lockfile(lockfile: Path, platform_filter: set[str] | None = None) -> set[WheelSpec]:
-    """
-    Parse `uv`'s TOML lockfile and attempt to extract wheel information for all resolved packages.
+        If `exclude_editable` is `True`, editable package installations (e.g. the project being
+        worked on) are ignored when building the final output.
+        """
+        with lockfile.open("rb") as f:
+            lf = tomllib.load(f)
 
-    By default, only cross-platform wheels are extracted from the lockfile. `platform_filter` may be
-    specified, as a set of platform tag strings, to include wheels for additional platforms if
-    available.
+        packages = set()
+        for p in lf["package"]:
+            if exclude_editable and ("editable" in p["source"]):
+                continue
 
-    NOTE: Packages that only provide source distributions are not currently considered.
-    """
-    with lockfile.open("rb") as f:
-        lf = tomllib.load(f)
+            packages.add(cls.from_lock(p))
 
-    keep_wheels = set()
-    for p in lf["package"]:
-        # TODO: Probably need to provide sdist-only as well
-        for w in p.get("wheels", []):
-            spec = WheelSpec.from_url(w["url"])
-            if "any" in spec.platforms:
-                keep_wheels.add(spec)
-
-            if platform_filter is not None:
-                if spec.platforms & platform_filter:
-                    keep_wheels.add(spec)
-
-    return keep_wheels
+        return packages
