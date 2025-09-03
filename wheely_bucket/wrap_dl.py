1434759e14c5de92686e58abd51895611f246264
S. Co1 <sco1.git@gmail.com>
2025-09-02 22:20:35 -0400

    Add `PackageSpec` deconfliction helper
    See: #2

--- wheely_bucket/wrap_dl.py
+++ wheely_bucket/wrap_dl.py
@@ -1,9 +1,37 @@
 import subprocess
 import sys
-from collections import abc
+from collections import abc, defaultdict
 from pathlib import Path
 
 from wheely_bucket.parse_lockfile import PackageSpec
+
+
+def split_conflicting(packages: abc.Iterable[PackageSpec]) -> abc.Iterator[list[PackageSpec]]:
+    """
+    Split the provided `PackageSpec`s into the largest possible non-conflicting chunks.
+
+    Because `pip download` attempts to resolve the dependencies passed, multiple locked versions of
+    the same package will result in a `ResolutionImpossible` error.
+    """
+    mapped_packages = defaultdict(list)
+    for p in packages:
+        mapped_packages[p.package_name].append(p.locked_ver)
+
+    while True:
+        chunk = []
+        for pname, versions in mapped_packages.items():
+            if len(versions) > 1:
+                chunk.append(PackageSpec(package_name=pname, locked_ver=versions.pop()))
+
+        if chunk:
+            yield chunk
+        else:
+            break
+
+    yield [
+        PackageSpec(package_name=pname, locked_ver=versions.pop())
+        for pname, versions in mapped_packages.items()
+    ]
 
 
 def pip_dl(
@@ -23,6 +51,11 @@ def pip_dl(
 
     NOTE: `--only-binary=:all:` will always be passed to the invoked CLI command; sdists are
     currently not considered by this tool.
+
+    NOTE: `packages` is assumed to not contain conflicting dependency specifications, e.g.
+    `abc==0.1.0` and `abc==0.2.0`, which will cause `pip` to throw a `ResolutionImpossible` error.
+    Download of conflicting packages can still be done into the same destination folder, but must be
+    done in separate calls.
     """
     pip_cmd = [
         sys.executable,
